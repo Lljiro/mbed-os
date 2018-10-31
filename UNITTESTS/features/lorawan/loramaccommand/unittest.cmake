479810a8f9e58ba7b1f15f0dca81f58787fb6bd5
Antti Kauppila <antti.kauppila@arm.com>
2018-10-31 12:40:54 +0200

    LoRaWAN: Moved keys to LoRaMacCrypto class
    - Unittests updated to reflect LoRaWAN 1.1 changes

--- UNITTESTS/features/lorawan/loramaccommand/unittest.cmake
+++ UNITTESTS/features/lorawan/loramaccommand/unittest.cmake
@@ -1,5 +1,5 @@
-/*
- * Copyright (c) , Arm Limited and affiliates.
+#[[
+ * Copyright (c) 2018, Arm Limited and affiliates
  * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,15 +13,31 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+]]
 
+# Unit test suite name
+set(TEST_SUITE_NAME "lorawan_LoRaMacCommand")
 
-#include <inttypes.h>
+# Source files
+set(unittest-sources
+  ../features/lorawan/lorastack/mac/LoRaMacCommand.cpp
+)
 
-#include "system/lorawan_data_structures.h"
+# Add test specific include paths
+set(unittest-includes ${unittest-includes}
+  target_h
+  ../features/lorawan/lorastack/mac
+)
 
-namespace LoRaMacCrypto_stub {
-extern int int_table_idx_value;
-extern int int_table[20];
-extern lorawan_status_t status;
-}
+# Test & stub files
+set(unittest-test-sources
+  features/lorawan/loramaccommand/Test_LoRaMacCommand.cpp
+  stubs/mbed_assert_stub.cpp
+  stubs/LoRaPHY_stub.cpp
+)
+
+set(unittest-test-flags
+  -DMBED_CONF_LORA_TX_MAX_SIZE=255
+  -DMBED_CONF_LORA_VERSION=0
+)
+
